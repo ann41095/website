0a96de3e7a78f7c7947cfdb8ef1c3f39af05fa4c
Andrii Ovsiannikov <Andrii_Ovsiannikov@epam.com>
2018-04-30 12:14:18 +0300

    Refactored servlet and paths

--- src/main/java/com/anya/LoginServlet.java
+++ src/main/java/com/anya/LoginServlet.java
@@ -10,22 +10,16 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Optional;
 
-@WebServlet (name = "HelloServlet",
-        urlPatterns = {"/hello"},
+@WebServlet (name = "LoginServlet",
+        urlPatterns = {"/login"},
         loadOnStartup = 1)
-public class HelloServlet extends HttpServlet {
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("landing.html").forward(req,resp);
-    }
-
+public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setAttribute(
                 "username", Optional.ofNullable(request.getParameter("username"))
                         .filter(StringUtils::isNotBlank).orElse("Anonymous")
         );
-        request.getRequestDispatcher("index.jsp").forward(request,response);
+        request.getRequestDispatcher("welcome.jsp").forward(request,response);
     }
 }
