406ebf8a0271aaac9172726709a36d61e93df36d
ann41095 <flatronl1917s@mail.ru>
2018-04-30 11:07:04 +0300

    404

--- src/main/java/com/anya/HelloServlet.java
+++ src/main/java/com/anya/HelloServlet.java
@@ -1,18 +1,28 @@
 package com.anya;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.*;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Optional;
 
 @WebServlet (name = "HelloServlet",
-        urlPatterns = {"/index"},
+        urlPatterns = {"/"},
         loadOnStartup = 1)
 public class HelloServlet extends HttpServlet {
+
     @Override
-    public void doGet (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-      request.setAttribute("message", "Ann");
-      request.getRequestDispatcher("index.jsp").forward(request,response);
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("landing.html").forward(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute(
+                "username", Optional.ofNullable(request.getParameter("username")).orElse("Anonymous")
+        );
+        request.getRequestDispatcher("index.jsp").forward(request,response);
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,8 @@
 <html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html" charset="ISO-8859-1">
+</head>
 <body>
-<h2>Hello, ${message}!</h2>
+<h2>Welcome, ${username}!</h2>
 </body>
 </html>
