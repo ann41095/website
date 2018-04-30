85dcd7279ec9bbd694c038c795c2571d7780ed9f
Andrii Ovsiannikov <Andrii_Ovsiannikov@epam.com>
2018-04-30 11:23:31 +0300

    Name submission

--- src/main/java/com/anya/HelloServlet.java
+++ src/main/java/com/anya/HelloServlet.java
@@ -1,5 +1,7 @@
 package com.anya;
 
+import org.apache.commons.lang3.StringUtils;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,7 +11,7 @@ import java.io.IOException;
 import java.util.Optional;
 
 @WebServlet (name = "HelloServlet",
-        urlPatterns = {"/"},
+        urlPatterns = {"/hello"},
         loadOnStartup = 1)
 public class HelloServlet extends HttpServlet {
 
@@ -21,7 +23,8 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setAttribute(
-                "username", Optional.ofNullable(request.getParameter("username")).orElse("Anonymous")
+                "username", Optional.ofNullable(request.getParameter("username"))
+                        .filter(StringUtils::isNotBlank).orElse("Anonymous")
         );
         request.getRequestDispatcher("index.jsp").forward(request,response);
     }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,7 +1,4 @@
 <html>
-<head>
-    <meta http-equiv="Content-Type" content="text/html" charset="ISO-8859-1">
-</head>
 <body>
 <h2>Welcome, ${username}!</h2>
 </body>
